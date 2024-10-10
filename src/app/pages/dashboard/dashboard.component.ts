de9a56d7235233a0569e5a3e04d4ffd44d566431
vinicius valentim <viniciusvalentim769@gmail.com>
2024-10-10 17:59:13 -0300

    adicionando dropdown ao botao que adiciona despesas, receitas e investimentos

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,17 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FoccoService } from '../../services/focco.service';
+import $ from 'jquery';
+import 'select2';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  dropdown: HTMLElement | null = null;
+  currentMonth!: number;
 
-  /**
-   *
-   */
+  constructor() {
+    this.currentMonth = new Date().getMonth() + 1; 
+  }
+
+
+  ngOnInit(): void {
+    this.dropdown = document.querySelector('.dropdown');
+
+    if (this.dropdown) {
+      this.dropdown.addEventListener('click', () => {
+        this.dropdown?.classList.toggle('active');
+      });
+    }
+
+
+  }
+
 
 }
